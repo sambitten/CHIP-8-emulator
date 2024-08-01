5b654eb949d414bdb9616890e07dba3349de7ac7
Sam Bitten <sambitten@hotmail.co.uk>
2024-08-01 13:57:11 +0100

    Added function to get next opcode

--- main.cpp
+++ main.cpp
@@ -17,9 +17,9 @@ WORD m_AddressI ; // the 16-bit address register
 WORD m_ProgramCounter ; // the 16-bit program counter
 std::vector m_Stack; // the 16-bit stack
 
+BYTE m_ScreenData[64][32];
 
-void CPUReset( )
-{
+void CPUReset() {
    m_AddressI = 0 ;
    m_ProgramCounter = 0x200 ;
    memset(m_Registers,0,sizeof(m_Registers)) ; // setting registers to 0
@@ -27,6 +27,15 @@ void CPUReset( )
    // load in the game
    FILE *in;
    in = fopen( "c:/INVADERS", "rb" ); //game location
-   fread( &m_GameMemory[0x200], 0xfff, 1, in); //
+   fread( &m_GameMemory[0x200], 0xfff, 1, in); 
    fclose(in);
+}
+
+WORD GetNextOpcode() {
+   WORD res = 0 ; //0000000000000000
+   res = m_GameMemory[m_ProgramCounter] ; 
+   res <<= 8 ;
+   res |= m_GameMemory[m_ProgramCounter+1] ;
+   m_ProgramCounter+=2 ; //add 2 as opcode is 2 bytes
+   return res ;
 }
