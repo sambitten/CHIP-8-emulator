b257d8df960da5bc0bf3f1fa861169da5fcb8a02
Sam Bitten <sambitten@hotmail.co.uk>
2024-08-04 14:56:43 +0100

    Cleaned syntax

--- chipCPU.h
+++ chipCPU.h
@@ -17,23 +17,18 @@ class ChipCPU
     ~ChipCPU();
     static ChipCPU* CreateSingleton();
     
-	void ExecuteNextOpcode();
+	  void ExecuteNextOpcode();
 
-	BYTE m_ScreenData[320][640][3] ; 
+	  BYTE m_ScreenData[320][640][3] ; 
 
   private:
     ChipCPU();
-
-	WORD GetNextOpcode();
-    
-
+	  WORD GetNextOpcode();
     void CPUReset();
-	void ClearScreen();
+	  void ClearScreen();
     void disp_clear();
-
     void Opcode0(WORD opcode);
     void Opcode00EE();
-
 
     static ChipCPU* m_Instance;
 
@@ -41,8 +36,7 @@ class ChipCPU
     BYTE m_Registers[16];
     WORD m_AddressI;
     WORD m_ProgramCounter;
-	std::vector<WORD> m_Stack;
-			
+	  std::vector<WORD> m_Stack;			
 };
 
 
